4268e2f6c003cd43b76199a8ac6fe7b2338ddc8a
moisesoliveira475 <moises475oliveira@gmail.com>
2024-02-11 12:44:32 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
+
+public class GetCurrentAuctionUseCase
+{
+    public Auction? Execute()
+    {
+        var repositorie = new RocketseatAuctionDbContext();
+
+        return repositorie
+            .Auctions
+            .Include(auction => auction.Items)
+            .FirstOrDefault();
+    }
+}
